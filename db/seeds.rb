2f6e8847fbcb44fc87d28f7dd19c9010d5dd0630
Juri Kato <dance.luv48@gmail.com>
2023-07-01 16:49:36 +0900

    working on articles app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Destroying articles..."
+Article.destroy_all
+puts "Done!"
+
+puts "Creating 10 articles..."
+10.times do
+res = Article.create!(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph
+  )
+  puts "#{res.title} created"
+end
+puts "Done"
